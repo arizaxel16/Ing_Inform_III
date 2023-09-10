74728310d78479e34c2e7f76236efaebbc8c6373
arizaxel16 <110404509+arizaxel16@users.noreply.github.com>
2023-09-09 22:13:57 -0500

    bank password handler updated
    correct password handler missing

--- Quiz_I_PNT/Bank/indexBank.js
+++ Quiz_I_PNT/Bank/indexBank.js
@@ -1,14 +1,53 @@
+var password = {};
+var strikes = 3;
+
+window.onload = function () {
+  for (let i = 0; i < 4; i++) {
+    password[i] = Math.floor(Math.random() * 10);
+  }
+
+  for (let i = 0; i < 4; i++) {
+    console.log(password[i]);
+  }
+};
+
 function moveToNextInput(currentInput, nextInputId) {
-    let inputValue = currentInput.value;
+  let inputValue = currentInput.value;
 
-    inputValue = inputValue.replace(/[^0-9]/g, '');
+  inputValue = inputValue.replace(/[^0-9]/g, "");
 
-    currentInput.value = inputValue;
+  currentInput.value = inputValue;
 
-    if (inputValue.length === 1) {
-      const nextInput = document.getElementById(nextInputId);
-      if (nextInput) {
-        nextInput.focus();
-      }
+  if (inputValue.length === 1) {
+    const nextInput = document.getElementById(nextInputId);
+    if (nextInput) {
+      nextInput.focus();
     }
   }
+}
+
+function onClickHandler() {
+  if (match()) {
+    strikes = 3;
+  } else {
+    strikes--;
+    document.getElementById("container_error").style.backgroundColor = "white";
+    document.getElementById("container_error").innerHTML =
+      "<h4>La contraseña es incorrecta</h4><h5 id='error_msg'></h5>";
+    document.getElementById("error_msg").innerText =
+      "Intentos restantes: " + strikes;
+    if (strikes === 0) {
+      window.close();
+    }
+  }
+}
+
+function match() {
+  for (let i = 0; i < 4; i++) {
+    if (password[i] != document.getElementById("num" + i).value) {
+      return false;
+    }
+  }
+
+  return true;
+}
