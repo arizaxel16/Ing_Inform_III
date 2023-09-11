b546b486ee11d4850a2d5921bb7bfb9a8b1e8211
arizaxel16 <110404509+arizaxel16@users.noreply.github.com>
2023-09-10 22:23:51 -0500

    transaction logic && receipt history

--- Quiz_I_PNT/Bank/logInBank.js
+++ Quiz_I_PNT/Bank/logInBank.js
@@ -1,26 +1,63 @@
 var balance = 0;
 var balanceText = document.getElementById("balance");
-
-function onClickHandleTransaction(transactionType) {
-    let transactionValue = parseInt(document.getElementById("txtField_transaction").value);
-    console.log("transaction value : "+transactionValue);
-    if (transactionType === 0) {
-        transactionValue = transactionValue*-1;
-    }
-    balance = parseInt(balance) + transactionValue;
-    console.log(balance);
-    balanceText.innerText = "";
-    balanceText.innerText = balance;
-
-    inputField.value = "";
-}
-
 var inputField = document.getElementById("txtField_transaction");
 
-inputField.addEventListener("input", function() {
+function onClickHandleTransaction(transactionType) {
+  let transactionValue = document.getElementById("txtField_transaction").value;
+
+  console.log(transactionValue);
+
+  if (transactionValue != "" && transactionValue != 0) {
+    transactionValue = parseInt(transactionValue);
+    if (transactionType === 1) {
+      updateText(transactionValue);
+      registerTransaction(transactionValue);
+    } else if (transactionType === 0 && transactionValue <= balance) {
+      transactionValue = transactionValue * -1;
+      updateText(transactionValue);
+      registerTransaction(transactionValue);
+    } else {
+      alert("You can't withdraw more than your current balance");
+    }
+  }
+}
+
+function registerTransaction(value) {
+  const withdrawHTML =
+    "<div class='container_receipt'><h3 class='negative'>Withdraw: $transactionValue</h3><h4>transactionTime</h4></div>";
+  const depositHTML =
+    "<div class='container_receipt'><h3 class='positive'>Deposit: $transactionValue</h3><h4>transactionTime</h4></div>";
+  let receipt;
+
+  let historyContainer = document.getElementById("container_history");
+
+  if (value > 0) {
+    receipt = depositHTML;
+  } else {
+    receipt = withdrawHTML;
+  }
+
+  receipt = receipt.replace("transactionValue", value);
+
+  let currentDate = new Date();
+  let dateString = currentDate.toLocaleDateString();
+  let timeString = currentDate.toLocaleTimeString();
+  let dateTimeString = dateString + " at " + timeString;
+
+  receipt = receipt.replace("transactionTime", dateTimeString);
+
+  historyContainer.innerHTML += receipt;
+}
+
+function updateText(transactionValue) {
+  balance = parseInt(balance) + transactionValue;
+  balanceText.innerText = "";
+  balanceText.innerText = balance;
+  inputField.value = "";
+}
+
+inputField.addEventListener("input", function () {
   var inputValue = inputField.value;
-
   inputValue = inputValue.replace(/\D/g, "");
-
   inputField.value = inputValue;
 });
